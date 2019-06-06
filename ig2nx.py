a406d5bd1077bd0750ab78444f2eadedc0c33fe8
ChronosLYH <32681491+ChronosLYH@users.noreply.github.com>
2019-06-06 20:10:17 +0800

    Initial commit

---
+++ ig2nx.py
@@ -0,0 +1,89 @@
+import igraph as ig 
+
+def minimum_cut(G, S_Node, T_Node):
+    G = G.G
+    S = G.vs.find(S_Node).index
+    T = G.vs.find(T_Node).index
+    cut = G.st_mincut(S, T, capacity='capacity')
+    partition0 = set(map(lambda index:eval(G.vs[index]['name'], {'S':'S', 'T':'T'}), cut[0]))
+    partition1 = set(map(lambda index:eval(G.vs[index]['name'], {'S':'S', 'T':'T'}), cut[1]))
+    return cut.value, (partition0, partition1)
+
+
+class DiGraph():
+    def __init__(self):
+        self.G = ig.Graph(directed=True)
+        self.add_node_queue = []
+        self.remove_node_queue = []
+        self.add_edge_queue = []
+        self.remove_edge_queue = []
+        self.BUFFER_MODE = True
+
+    def flush(self):
+        if self.BUFFER_MODE:
+            if self.remove_edge_queue:
+                self.G.delete_edges(self.remove_edge_queue)
+                self.remove_edge_queue.clear()
+            if self.add_node_queue:
+                self.G.add_vertices(self.add_node_queue)
+                self.add_node_queue.clear()
+            if self.remove_node_queue:
+                self.G.delete_vertices(self.remove_node_queue)
+                self.remove_node_queue.clear()
+            if self.add_edge_queue:
+                add_edge_queue_with_index = []
+                for u, v, capacity in self.add_edge_queue:
+                    index_u = self.G.vs.find(u).index
+                    index_v = self.G.vs.find(v).index
+                    add_edge_queue_with_index.append( (index_u, index_v, capacity) )
+                start = len(self.G.es)
+                self.G.add_edges(map(lambda tup:tup[:-1], add_edge_queue_with_index))
+                end = len(self.G.es)
+                self.G.es[start:end]['capacity'] = list(map(lambda tup:tup[-1], self.add_edge_queue))
+                self.add_edge_queue.clear()
+            
+        else:
+            pass
+
+    def add_node(self, node):
+        node = str(node)
+        if self.BUFFER_MODE:
+            self.add_node_queue.append(node)
+        else:
+            self.G.add_vertex(node)
+
+    def add_nodes_from(self, list_of_nodes):
+        #list_of_nodes = map(self._generate_index, list_of_nodes)
+        list_of_nodes = map(str, list_of_nodes)
+        self.G.add_vertices(list_of_nodes)
+    
+    def remove_node(self, node):
+        #index = self.name2index.pop(node)
+        #self.index2name.pop(index)
+        node = str(node)
+        index = self.G.vs.find(node).index
+        if self.BUFFER_MODE:
+            self.remove_node_queue.append(index)
+        else:
+            self.G.delete_vertices(index)
+
+    def add_edge(self, u, v, capacity):
+        u, v = str(u), str(v)
+        
+        if capacity == float('inf'):
+            capacity = 9999
+        else:
+            capacity = int(capacity)
+        if self.BUFFER_MODE:
+            self.add_edge_queue.append( (u, v, capacity) )
+        else:
+            index_u, index_v = self.G.vs.find(u).index, self.G.vs.find(v).index
+            self.G.add_edge(index_u, index_v, capacity=capacity)
+    def remove_edge(self, u, v):
+        u, v = str(u), str(v)
+        index_u, index_v = self.G.vs.find(u).index, self.G.vs.find(v).index
+        edgeID = self.G.get_eid(index_u, index_v, directed=True)
+        if self.BUFFER_MODE:
+            self.remove_edge_queue.append(edgeID)
+        else:
+            self.G.delete_edges(edgeID)
---
+++ video.py
@@ -0,0 +1,311 @@
+from PIL import Image
+import numpy as np
+import ig2nx as nx
+import matplotlib.pyplot as plt
+import itertools
+from operator import itemgetter
+import collections
+from copy import deepcopy
+import imageio
+import time
+
+
+def L1Norm(pixel1, pixel2):
+    return sum( (I1-I2 if I1>I2 else I2-I1) for I1, I2 in zip(pixel1, pixel2)) 
+    
+class VideoWrapper():
+    def __init__(self, img):
+        self.originalPixels = img
+        self.augmentedPixels = {}
+    def getValue(self, node):
+        if isinstance(node[0], int):
+            return self.originalPixels[node[0]][node[1]][node[2]]
+        else:
+            return self.augmentedPixels[node]
+    def setValue(self, node, value):
+        if isinstance(node[0], int):
+            self.originalPixels[node[0]][node[1]][node[2]] = value
+        else:
+            self.augmentedPixels[node] = value
+    
+    
+class VideoSeamCarver():
+    # Node 和 img 有一一对应关系
+    # Pos 是删除Seam后Node的位置
+    def __init__(self, img, mode='vertical'):
+        self.img = VideoWrapper(img)
+        self.numFrames = img.shape[0]
+        self.numRows = img.shape[1]
+        self.numCols = img.shape[2]
+        self.Pos2Node = [[[(t, i, j) for j in range(self.numCols)] for i in range(self.numRows)] for t in range(self.numFrames)]
+        self.Energy = L1Norm
+        self.Node2Img = lambda node: self.img.getValue(node) # returns pixel value
+        self.Pos2Img = lambda t, i, j: self.Node2Img(self.Pos2Node[t][i][j]) # returns pixel value
+        self._initializeGraph()
+        self.PALETTE = {
+            'black': np.asarray([0, 0, 0], np.uint8), 
+            'red': np.asarray([255, 0, 0], np.uint8)
+        }
+
+    def _initializeGraph(self):
+        startTime = time.time()
+        self.G = nx.DiGraph()
+        self.S_Node = 'S'
+        self.T_Node = 'T'
+        self.G.add_nodes_from([self.S_Node, self.T_Node])
+        for j in range(self.numCols):
+            for t in range(self.numFrames):
+                for i in range(self.numRows):
+                    currentNode = self.Pos2Node[t][i][j]
+                    self.G.add_node(currentNode)
+
+                    self._addEdges(t, i, j)
+        self.G.flush()
+        print('Graph Initialization took %s seconds' % (time.time()-startTime))
+                
+    def _addEdges(self, t, i, j):
+        '''
+        将Pos为(t, i, j)的Node和与其在Pos意义上相邻的Node，在Graph中连接
+        '''
+        currentNode = self.Pos2Node[t][i][j]
+
+        if j == 0:
+            self.G.add_edge(self.S_Node, currentNode, capacity=float('inf'))
+        elif j == len(self.Pos2Node[t][i]) - 1:
+            self.G.add_edge(currentNode, self.T_Node, capacity=float('inf'))
+
+        backwardNode = self._getBackwardNode(t, i, j)
+        if backwardNode is not None:
+            self.G.add_edge(backwardNode, currentNode, capacity=self.Energy(self.Node2Img(backwardNode), self.Node2Img(currentNode)))
+            self.G.add_edge(currentNode, backwardNode, capacity=float('inf'))
+        
+        for diagonalNode in self._getDiagonalNodes(t, i, j):
+            self.G.add_edge(currentNode, diagonalNode, capacity=float('inf'))
+    def _removeEdges(self, t, i, j):
+        currentNode = self.Pos2Node[t][i][j]
+
+        if j == 0:
+            self.G.remove_edge(self.S_Node, currentNode)
+        elif j == len(self.Pos2Node[t][i]) - 1:
+            self.G.remove_edge(currentNode, self.T_Node)
+
+        backwardNode = self._getBackwardNode(t, i, j)
+        if backwardNode is not None:
+            self.G.remove_edge(backwardNode, currentNode)
+            self.G.remove_edge(currentNode, backwardNode)
+        
+        for diagonalNode in self._getDiagonalNodes(t, i, j):
+            self.G.remove_edge(currentNode, diagonalNode)
+
+    def _getBackwardNode(self, t, i, j):
+        if j > 0:
+            return self.Pos2Node[t][i][j-1]
+        else:
+            return None
+    def _getDiagonalNodes(self, t, i, j):
+        result = []
+        if j > 0:
+            if i > 0: result.append(self.Pos2Node[t][i-1][j-1])
+            if i < self.numRows - 1: result.append(self.Pos2Node[t][i+1][j-1])
+            if t > 0: result.append(self.Pos2Node[t-1][i][j-1])
+            if t < self.numFrames - 1: result.append(self.Pos2Node[t+1][i][j-1])
+        return result
+    def _getAffectedPoss(self, t, i, j):
+        result = []
+        if j+1 < len(self.Pos2Node[t][i]):
+            result.append((i, j+1))
+        if j+2 < len(self.Pos2Node[t][i]):
+            result.append((i, j+2))
+        return result
+
+    def Solve(self):
+        '''根据当前Graph结构，返回最小割'''
+        startTime = time.time()
+        cutValue, partition = nx.minimum_cut(self.G, self.S_Node, self.T_Node)
+        print('Solving Minimum Cut: %s seconds' % (time.time()-startTime) )
+        leftPartition, rightPartition = partition
+        leftPartition.remove('S')
+        seam = [[-1 for i in range(self.numRows)] for t in range(self.numFrames)]
+        '''
+        for row, col in leftPartition:
+            if col > seam[row]:
+                seam[row] = col  # Group By Row, Max of Col
+        '''
+        for t in range(self.numFrames):
+            for i in range(self.numRows):
+                for j in range(len(self.Pos2Node[t][i])-1, -1, -1):
+                    if self.Pos2Node[t][i][j] in leftPartition:
+                        seam[t][i] = (t, i, j)
+                        break
+        return seam # Contains Positions
+
+    def SolveK(self, k):
+        '''根据当前Graph结构，返回前K个最小割'''
+        raise NotImplementedError()
+        tempG = deepcopy(self.G)
+        seams = []
+        for _ in range(k):
+            cutValue, partition = nx.minimum_cut(tempG, self.S_Node, self.T_Node)
+            leftPartition, rightPartition = partition
+            leftPartition.remove('S')
+            seam = collections.defaultdict(int)
+
+            for i in range(self.numRows):
+                for j in range(len(self.Pos2Node[i])-1, -1, -1):
+                    if self.Pos2Node[i][j] in leftPartition:
+                        tempG.add_edge(self.Pos2Node[i][j], self.Pos2Node[i][j+1], capacity=float('inf'))
+                        seam[t][i] = (t, i, j)
+                        break
+            seams.append(seam)
+        return seams
+    
+    def RemoveSeam(self, seam):
+        '''削除Seam处的一列像素，并对Graph结构进行更新'''
+        addEdgesQueue = []
+        seam = list(itertools.chain(*seam))
+        for frame, row, j in seam:
+            t = frame
+            i = row
+            node = self.Pos2Node[frame][row].pop(j)
+            self.G.remove_node( node ) # Remove from Pos2Node and G, the seam node in row i. Then Pos2Node[i][j] becomes Pos2Node[i][j+1]
+            if j < len(self.Pos2Node[t][i]): # if Pos2Node[i][j+1] not out of bound
+                addEdgesQueue.append((t, i, j))
+        for t, i, j in addEdgesQueue:
+            self._addEdges(t, i, j) 
+        self.G.flush()
+
+    def AugmentSeam(self, seam):
+        '''在Seam处填充一列像素，并对Graph结构进行更新'''
+        insertNodeQueue = []
+        addEdgesQueue = []
+        for frame, row, j in seam:
+            t = frame
+            i = row
+            
+            if j+1 < len(self.Pos2Node[t][i]): # if Pos2Node[i][j+1] not out of bound
+                augmentation = ( self.Pos2Img(t, i, j)/2 + self.Pos2Img(t, i, j+1)/2 ).astype('uint8') # Averaging
+                augmentedNode = (self.Pos2Node[t][i][j], self.Pos2Node[t][i][j+1])
+                self.img.setValue(augmentedNode, augmentation)
+                self.G.add_node(augmentedNode)
+                
+                for _t, _i, _j in self._getAffectedPoss(t, i, j):
+                    self._removeEdges(_t, _i, _j) 
+                insertNodeQueue.append((t, i, j, augmentedNode))
+                
+            else: # Pos2Node[i][j] connected with T_Node, this should never happen
+                raise Exception('??')
+        for t, i, j, augmentedNode in insertNodeQueue:
+            self.Pos2Node[t][i].insert(j+1, augmentedNode) # Insert Between j and j+1, Node at j+1 changed to augmentedNode
+            addEdgesQueue.append((t, i, j+1))
+            for _t, _i, _j in self._getAffectedPoss(t, i, j+1):
+                addEdgesQueue.append((_t, _i, _j))
+        for t, i, j in addEdgesQueue:
+            self._addEdges(t, i, j) 
+        
+        self.G.flush()
+            
+
+    def ShowGraph(self):
+        raise NotImplementedError()
+        '''展示Graph结构'''
+        pos = {self.Pos2Node[i][j]:(i, j) for j in range(len(self.Pos2Node[i])) for i in range(self.numRows)}
+        pos['S'] = (0, -1)
+        pos['T'] = (0, len(self.Pos2Node[0]))
+        labels = nx.get_edge_attributes(self.G, 'capacity')
+        nx.draw_networkx_edges(self.G, pos=pos)
+        nx.draw_networkx_edge_labels(self.G, pos=pos, edge_labels=labels, font_size=8, label_pos=0.3)
+        nx.draw_networkx_nodes(self.G, pos=pos)
+        nx.draw_networkx_labels(self.G, pos=pos)
+        plt.show()
+
+    def GenerateVideoWithSeam(self, seam, numCols):
+        '''
+        根据当前Pos2Node二维数组构造图片
+
+        returns numpy array
+        '''
+        videoWithSeam = self.GenerateVideo()
+        seam = list(itertools.chain(*seam))
+        for t, i, j in seam:
+            videoWithSeam[t][i][j] = self.PALETTE['red']
+        return videoWithSeam
+    def GenerateImgWithSeams(self, seams):
+        '''
+        根据当前Pos2Node二维数组构造图片
+
+        returns numpy array
+        '''
+        videoWithSeams = self.GenerateVideo()
+        for seam in seams:
+            for t, i, j in seam:
+                videoWithSeams[t][i][j] = self.PALETTE['red']
+        return videoWithSeams
+    def GenerateVideo(self):
+        '''
+        根据当前Pos2Node二维数组构造图片
+        
+        returns numpy array
+        '''
+        video = np.asarray([[
+                            [self.Node2Img(node)
+                            for node in row]
+                        for row in frame]
+                        for frame in self.Pos2Node])
+        return video
+    def ShowImg(self, seam, numCols):
+        raise NotImplementedError()
+        imgWithSeam = self.GenerateImgWithSeam(seam, numCols)
+
+        plt.imshow(imgWithSeam)
+        plt.show()
+
+if __name__ == '__main__':
+    IMAGE_AS_VIDEO, SMALL_DATA, REMOVE_SEAM_TEST, AUGMENT_SEAM_TEST = False, False, True, False
+    REMOVE_SEAMS_COUNT = 40
+    
+    if IMAGE_AS_VIDEO:
+        img = Image.open('2.png')
+        img = img.convert('RGB')
+        img = np.array(img)
+        video = np.reshape(img, [1, *img.shape])
+    else:
+        video = imageio.get_reader('golf.mov', 'ffmpeg')
+        frames = []
+        for i, frame in enumerate(video):
+            frames.append(frame)
+        video = np.array(frames)
+
+    if SMALL_DATA:
+        video = video[:10, ...]
+    carver = VideoSeamCarver(video)
+    #carver.Draw()
+
+    if REMOVE_SEAM_TEST: # 测试减少图片宽度的功能
+        videos = []
+        for i in range(REMOVE_SEAMS_COUNT):
+            startTime = time.time()
+            print(i)
+            seam = carver.Solve()
+            #carver.ShowImg(seam)
+            video = carver.GenerateVideoWithSeam(seam, carver.numCols+20)
+            imageio.mimsave('video/%s.gif'% i, video)
+            videos.append(video)
+            carver.RemoveSeam(seam)
+            print('Total Time Removing Seam %s/%s: %s seconds'% (i, REMOVE_SEAMS_COUNT, time.time()-startTime))
+        video = carver.GenerateVideo()
+        imageio.mimsave('video/result.gif', video)
+        
+        #carver.ShowImg(None)
+
+    if AUGMENT_SEAM_TEST: # 测试增加图片宽度的功能
+        seams = carver.SolveK(20)
+        imgWithSeams = carver.GenerateImgWithSeams(seams)
+        plt.imshow(imgWithSeams)
+        plt.show()
+
+        for seam in seams:
+            carver.AugmentSeam(seam)
+        imgResult = carver.GenerateVideo()
+        
+        plt.imshow(imgResult)
+        plt.show()
